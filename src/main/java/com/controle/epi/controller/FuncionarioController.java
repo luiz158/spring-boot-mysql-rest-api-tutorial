6d8ff944cf21476efda1dc9e8a5d20061f4d735a
gabrieltorrees <gabriel-torres1997@hotmail.com>
2019-06-06 22:35:25 -0300

    teste

---
+++ src/main/java/com/controle/epi/controller/FuncionarioController.java
@@ -0,0 +1,62 @@
+package com.controle.epi.controller;
+
+import com.controle.epi.exception.ResourceNotFoundException;
+import com.controle.epi.model.Funcionario;
+import com.controle.epi.repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * Created by rajeevkumarsingh on 27/06/17.
+ */
+@RestController
+@RequestMapping("/api")
+public class FuncionarioController {
+
+    @Autowired
+    FuncionarioRepository funcionarioRepository;
+
+    @GetMapping("/funcionarios")
+    public List<Funcionario> getAllFuncionarios() {
+        return funcionarioRepository.findAll();
+    }
+
+    @PostMapping("/funcionarios")
+    public Funcionario createFuncionario(@Valid @RequestBody Funcionario funcionario) {
+        return funcionarioRepository.save(funcionario);
+    }
+
+    @GetMapping("/funcionarios/{id}")
+    public Funcionario getFuncionarioById(@PathVariable(value = "id") Long funcionarioId) {
+        return funcionarioRepository.findById(funcionarioId)
+                .orElseThrow(() -> new ResourceNotFoundException("Funcionario", "idFuncionario", funcionarioId));
+    }
+
+    @PutMapping("/funcionarios/{id}")
+    public Funcionario updateFuncionario(@PathVariable(value = "id") Long funcionarioId,
+                                           @Valid @RequestBody Funcionario funcionarioDetails) {
+
+        Funcionario funcionario = funcionarioRepository.findById(funcionarioId)
+                .orElseThrow(() -> new ResourceNotFoundException("Funcionario", "idFuncionario", funcionarioId));
+
+//        funcionario.setTitle(noteDetails.getTitle());
+//        funcionario.setContent(noteDetails.getContent());
+
+        Funcionario updatedFuncionario = funcionarioRepository.save(funcionario);
+        return updatedFuncionario;
+    }
+
+    @DeleteMapping("/funcionarios/{id}")
+    public ResponseEntity<?> deleteFuncionario(@PathVariable(value = "id") Long funcionarioId) {
+        Funcionario funcionario = funcionarioRepository.findById(funcionarioId)
+                .orElseThrow(() -> new ResourceNotFoundException("Funcionario", "idFuncionario", funcionarioId));
+
+        funcionarioRepository.delete(funcionario);
+
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/controle/epi/model/Funcionario.java
@@ -0,0 +1,173 @@
+package com.controle.epi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+@Table(name = "funcionario")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"},
+        allowGetters = true)
+public class Funcionario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idFuncionario;
+
+    private String nome;
+    private String rua;
+    private int numero;
+    private String cidade;
+    private String estado;
+    private String pais;
+    private String cep;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String cargo;
+    private boolean admin;
+    private Date dataNascimento;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+
+    public Long getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(Long idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
